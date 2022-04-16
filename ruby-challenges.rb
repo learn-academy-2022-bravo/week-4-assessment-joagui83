4ed0b6735a73651614cc34d25e2588e387931a46
joagui83 <jjaggie83@gmail.com>
2022-04-16 12:45:55 -0700

    finished up ruby challenges and coding challenges

--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -10,20 +10,42 @@ num2 = 42
 num3 = 221
 # Expected output: '221 is odd'
 
+def odd_even_outcome(number)
+    if number.even?
+        puts "#{number} is even."      
+    elsif number.odd?
+        puts "#{number} is odd."
+    end
+end
 
+puts odd_even_outcome num3
 
 # -------------------2) Create a method that takes in a string and removes all the vowels from the string. Use the test variables provided. HINT: Check out this resource: https://ruby-doc.org/core-2.6/String.html#method-i-delete
+#create a method called palindrome with a parameter of string 
 
-album1 = 'Rubber Soul'
+
+#use the ruby built in method .delete to remove vowels from a string
+album1 = 'Rubber Soul' 
 # Expected output: 'Rbbr Sl'
 album2 = 'Sgt Pepper'
 # Expected output: 'Sgt Pppr'
 album3 = 'Abbey Road'
 # Expected output: 'bby Rd'
 
+def vowel_remover(string)
+    string.delete "aeiou"
+end
+
+puts vowel_remover album1
+
 
 
 # -------------------3) Create a method that takes in a string and checks if the string is a palindrome. A palindrome is the same word spelled forward or backward. Use the test variables provided.
+
+#create a method called palindrome with a parameter of string 
+#use the ruby built in method reverse to see if the string equals itself if reversed and add .downcase to account for capital letters
+
+
 
 palindrome_tester1 = 'Racecar'
 # Expected output: 'Racecar is a palindrome'
@@ -31,3 +53,13 @@ palindrome_tester2 = 'LEARN'
 # Expected output: 'LEARN is not a palindrome'
 palindrome_tester3 = 'Rotator'
 # Expected output: 'Rotator is a palindrome'
+
+def palindrome(string)
+    if string.downcase == string.downcase.reverse
+        puts "#{string} is a palindrome."      
+    else 
+        puts "#{string} is not a palindrome."
+    end
+end
+
+puts palindrome palindrome_tester3
